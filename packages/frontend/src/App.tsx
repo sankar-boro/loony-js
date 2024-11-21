0de27680b5ea6b0388be691bf36c35f3d2e97cb0
Sankar Boro <sankar.boro@yahoo.com>
2024-11-21 19:00:22 +0530

    working

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,53 +1,9 @@
-import React, { useEffect, useState } from "react";
 import "./App.css";
+import AudioRecorder from './AudioRecorder'
 
 function App() {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [message, setMessage] = useState("");
-  const [receivedMessages, setReceivedMessages] = useState<any[]>([]);
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    // Create WebSocket connection
-    const ws = new WebSocket("ws://localhost:5000/ws"); // Replace with your WebSocket URL
-
-    // Set the WebSocket instance
-    setSocket(ws);
-
-    // Event listener for when the WebSocket connection is opened
-    ws.onopen = () => {
-      console.log("WebSocket connection opened");
-    };
-
-    // Event listener for receiving messages from the server
-    ws.onmessage = (event) => {
-      console.log("Message received from server:", event.data);
-      setReceivedMessages((prevMessages: any) => [...prevMessages, event.data]);
-    };
-
-    // Event listener for WebSocket errors
-    ws.onerror = (error) => {
-      console.error("WebSocket error:", error);
-    };
-
-    // Event listener for WebSocket close
-    ws.onclose = () => {
-      console.log("WebSocket connection closed");
-    };
-
-    console.log(ws)
-  }, []);
-
   return (
-    <>
-      App
-      <button
-        onClick={() => {
-          socket && socket.send(JSON.stringify({ data: "hello" }));
-        }}
-      >
-        Click
-      </button>
-    </>
+    <AudioRecorder />  
   );
 }
 
---
+++ packages/frontend/src/AudioRecorder.tsx
@@ -0,0 +1,105 @@
+import { useState, useRef, useEffect } from 'react';
+import { useWebSocket } from 'loony-audio/useWebSocket';
+import WebAudioApi from 'loony-audio/index';
+import { encodeWAV } from 'loony-audio/encoder/wav';
+import "./Audio.css"
+
+function App() {
+  const { socket, isConnected, openWebSocket, closeWebSocket } =
+    useWebSocket('ws://127.0.0.1:3494/ws');
+  const audioRef = useRef<WebAudioApi | null>(null);
+  const [audioUrl, setAudioUrl] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const loonyAudio = await WebAudioApi.create();
+      audioRef.current = loonyAudio;
+    })();
+  }, []);
+
+  const onOpenWebSocket = () => {
+    openWebSocket();
+  };
+  const onCloseWebSocket = () => {
+    closeWebSocket();
+  };
+
+  const startRecording = () => {
+    audioRef.current?.connect();
+  };
+
+  const stopRecording = () => {
+    audioRef.current?.disconnect();
+  };
+
+  const startSocketRecording = () => {
+    audioRef.current?.socketConnect(socket.current as WebSocket);
+  };
+
+  const logData = async () => {
+    const audioBlob = new Blob([encodeWAV(audioRef.current?.buffer as Float32Array[], 44100)], {
+      type: 'audio/wav',
+    });
+    const audioUrl = URL.createObjectURL(audioBlob);
+    setAudioUrl(audioUrl);
+  };
+
+  const handleFileChange = (_: React.ChangeEvent<HTMLInputElement>) => {};
+  console.log(isConnected);
+
+  return (
+    <div className='ui-container'>
+      <div className='button-group'>
+        <h3>WebSocket Controls</h3>
+        <button className='btn' onClick={onOpenWebSocket}>
+          Open WebSocket
+        </button>
+        <button className='btn' onClick={onCloseWebSocket}>
+          Close WebSocket
+        </button>
+      </div>
+
+      <div className='button-group'>
+        <h3>Recording Controls</h3>
+        <button className='btn' onClick={startRecording}>
+          Start Recording
+        </button>
+        <button className='btn' onClick={stopRecording}>
+          Stop Recording
+        </button>
+        <button className='btn' onClick={logData}>
+          Log
+        </button>
+      </div>
+
+      <div className='button-group'>
+        <h3>Web socket Recording Controls</h3>
+        <button className='btn' onClick={startSocketRecording}>
+          Start Recording
+        </button>
+        <button className='btn' onClick={stopRecording}>
+          Stop Recording
+        </button>
+        <button className='btn' onClick={logData}>
+          Log
+        </button>
+      </div>
+
+      <div className='audio-section'>
+        {audioUrl && (
+          <>
+            <h3>Audio Playback</h3>
+            <audio controls src={audioUrl}></audio>
+          </>
+        )}
+      </div>
+
+      <div className='file-input'>
+        <h3>Upload File</h3>
+        <input type='file' onChange={handleFileChange} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
